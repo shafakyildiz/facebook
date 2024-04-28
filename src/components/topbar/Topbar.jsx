88fd8cc0404c6f536e083297e1de548e575e9a0a
shafakyildiz <shafakyildiz@gmail.com>
2024-04-28 23:19:22 +0300

    add topbar items

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,43 @@
 import React from "react";
 import "./topbar.css";
+import { Chat, Notifications, Person, Search } from "@material-ui/icons";
 
 export default function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="topbarCenter">Facebook</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search />
+          <input
+            placeholder="Search for a friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink"></span>
+          <span className="topbarLink"></span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="../../assets/person/1.jpg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
 }
