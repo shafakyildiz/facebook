1c933161dc2f68e6efcbf58b8d98ba2ab5de96a9
shafakyildiz <shafakyildiz@gmail.com>
2024-04-29 01:24:22 +0300

    complete share component

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,6 +1,6 @@
+import { EmojiEmotions, Label, PermMedia, Room } from "@material-ui/icons";
 import React from "react";
 import "./share.css";
-import { PermMedia } from "@material-ui/icons";
 
 export default function Share() {
   return (
@@ -22,10 +22,23 @@ export default function Share() {
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMedia />
+              <PermMedia htmlColor="red" className="shareIcon" />
               <span className="shareOptionText">Photo or video</span>
             </div>
+            <div className="shareOption">
+              <Label htmlColor="blue" className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <Room htmlColor="green" className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
+            </div>
           </div>
+          <button className="shareButton">Share</button>
         </div>
       </div>
     </div>
