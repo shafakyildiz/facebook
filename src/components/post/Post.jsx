7ff6467ca898045143ee8bfed2e377fcd0b9f8f1
shafakyildiz <shafakyildiz@gmail.com>
2024-04-29 11:01:38 +0300

    add post comenent

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -4,7 +4,18 @@ export default function Post() {
   return (
     <div className="post">
       <div className="postWrapper">
-        <div className="postTop"></div>
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src="../../assets/person/1.jpeg"
+              alt=""
+            />
+            <span className="postUsername">John Doe</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight"></div>
+        </div>
         <div className="postCenter"></div>
         <div className="postBottom"></div>
       </div>
