99aa8b7ccfc5337ba788c4efd3327d0a21b8c6fe
shafakyildiz <shafakyildiz@gmail.com>
2024-04-30 16:18:30 +0300

    create login page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Profile />;
+  return <Login />;
 }
 
 export default App;
