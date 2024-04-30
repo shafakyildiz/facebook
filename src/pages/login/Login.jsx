5f1217bb78fb16b47ccd9588734bd10e6059be34
shafakyildiz <shafakyildiz@gmail.com>
2024-04-30 16:07:32 +0300

    add login component

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft"></div>
+        <div className="loginRight"></div>
+      </div>
+    </div>
+  );
+}
