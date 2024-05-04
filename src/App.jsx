82a01dae8ba503759ce6e65f28c4056f3416cb22
shafakyildiz <shafakyildiz@gmail.com>
2024-05-04 15:29:50 +0300

    add router to appjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <Home />;
+  return <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="profile" element={<Profile />} />
+    <Route path="register" element={<Register />} />
+    <Route path="login" element={<Login />} />
+  </Routes>
 }
 
 export default App;
